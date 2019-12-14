aa1784af4e0d1e8444a1ab0b8d089fb2a9b153fa
evanzhengli <evanzhengli@tencent.com>
2019-12-14 10:24:38 +0800

    feat: add text render example

---
+++ src/components/TextRender/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label>
+      <input v-model="username">
+    </label>
+    <div v-if="error" class="error">
+      {{ error}}
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: ''
+    }
+  },
+  computed: {
+    error () {
+      return this.username.trim().length < 7
+        ? 'Please enter a longer username'
+        : ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <TextRender />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import TextRender from '@/components/TextRender'
+
+export default {
+  name: 'About',
+  components: {
+    TextRender
+  }
+}
+</script>
